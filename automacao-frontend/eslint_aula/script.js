7316aa54c67b63f57f8fdad7bae0ee447b5dd797
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-01-22 14:06:53 -0300

    Inicio da refatoração - branch

--- automacao-frontend/eslint_aula/script.js
+++
@@ -1,5 +0,0 @@
-const teste1 = 0;
-const teste2 = 1;
-
-console.log(teste1);
-console.log(teste2);
--- regular-expression/validador-cpf/script.js
+++
@@ -1,7 +0,0 @@
-import { ValidarCpf } from './validar-cpf.js';
-
-const cpf = document.querySelector('#cpf');
-
-const validarCpf = new ValidarCpf(cpf).iniciar();
-
-console.log(validarCpf);
