42a73ec139c4560f8ae6f8d9088559ab6ca0f290
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-01-23 18:32:13 -0300

    Adiciona navegação dos slides via interface por botões

--- slide/js/script.js
+++ slide/js/script.js
@@ -1,6 +1,8 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
 
-const initSlide = new Slide('.slide', '.slide-wrapper');
+const initSlide = new SlideNav('.slide', '.slide-wrapper');
+
 initSlide.init();
+initSlide.addArrow('.prev', '.next');
 
-initSlide.changeSlide(0);
+console.log(initSlide);
--- slide/js/slide.js
+++ slide/js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
@@ -142,7 +142,8 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
-
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
     this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
@@ -152,7 +153,20 @@ export default class Slide {
     this.addSlideEvents();
     this.slidesConfig();
     this.addResizeEvent();
-
+    this.changeSlide(0);
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 }
