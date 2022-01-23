313f58972bfeefb77cd26d28ee83dc7f7525426c
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-01-23 16:47:24 -0300

    estrutura inicial dos eventos do slide

---
+++ slide/js/script.js
@@ -0,0 +1,5 @@
+import Slide from './slide.js';
+
+const initSlide = new Slide('.slide', '.slide-wrapper');
+initSlide.init();
+console.log(initSlide);
---
+++ slide/js/slide.js
@@ -0,0 +1,39 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    console.log('mousedown');
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+    console.log('moveu');
+  }
+
+  onEnd(event) {
+    console.log('acabou');
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+
+    return this;
+  }
+}
